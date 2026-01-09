f8a171a2b9f7fa847574ec8ceb36642b3d1ce64f
Abadulhassan <45556780+Abadulhassan@users.noreply.github.com>
2026-01-09 14:07:57 +0500

    Update index.js
    code revamp

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-console.log("created for research purposes only")
+exports.handler = async (event) => {
+    console.log("Incoming event:", JSON.stringify(event));
 
----check this 
+    const response = {
+        message: "Hello from AWS Lambda!",
+        input: event
+    };
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(response)
+    };
+};
