48a8e5c7bf8d7f8e9a627784a1074d90e0e05eda
Abadulhassan <45556780+Abadulhassan@users.noreply.github.com>
2026-01-16 17:19:29 +0500

    Update index.js
    best code ever

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ exports.handler = async (event) => {
     console.log("Incoming event:", JSON.stringify(event));
 
     const response = {
-        message: "Hello from AWS Lambda!",
+        message: "Hello from AWS Lambda this best lambda responce ever!",
         input: event
+        
     };
 
     return {
